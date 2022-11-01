7e5866a67a1cc80753e9711ab519d12afc4acaa4
Nancy Awa <nancyawa05@gmail.com>
2022-10-31 18:19:11 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,8 @@
 * @src: is source memory
 * @n: number of bytes to be copied
 * Return: string copied from source
-**/
+*/
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {	
 	unsigned int a;
